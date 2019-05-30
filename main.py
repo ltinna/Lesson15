7212b477398c44ab4bd61cd77c72a1433a91baf8
Tina Lovsin <lovsin.tina@gmail.com>
2019-05-30 18:38:16 +0200

    Jinja template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
@@ -21,6 +21,18 @@ def about_me():
 @app.route("/portfolio")
 def portfolio():
     return render_template ("portfolio.html")
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
 
 
 if __name__ == '__main__':
